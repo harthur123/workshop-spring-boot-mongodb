6605bbf0c3b43c2adde9f4ed5277405acb9d1f5a
harthur <harthurhenrique214@gmail.com>
2025-07-25 20:04:41 -0300

    Connect to MongoDB with repository and service layers

---
+++ src/main/java/com/harthur/workshopmongo/services/UserService.java
@@ -0,0 +1,17 @@
+package com.harthur.workshopmongo.services;
+
+import com.harthur.workshopmongo.domain.User;
+import com.harthur.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repo;
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
