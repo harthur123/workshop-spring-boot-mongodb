ce817aa527a926e8f8b751b8b83773a20a22ced4
harthur <harthurhenrique214@gmail.com>
2025-07-31 21:43:29 -0300

    Get post by ID

---
+++ src/main/java/com/harthur/workshopmongo/resources/PostResource.java
@@ -0,0 +1,29 @@
+package com.harthur.workshopmongo.resources;
+
+import com.harthur.workshopmongo.domain.Post;
+import com.harthur.workshopmongo.domain.User;
+import com.harthur.workshopmongo.dto.UserDTO;
+import com.harthur.workshopmongo.services.PostService;
+import com.harthur.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/harthur/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.harthur.workshopmongo.services;
+
+import com.harthur.workshopmongo.domain.Post;
+
+import com.harthur.workshopmongo.repository.PostRepository;
+import com.harthur.workshopmongo.repository.UserRepository;
+import com.harthur.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+    }
+}
